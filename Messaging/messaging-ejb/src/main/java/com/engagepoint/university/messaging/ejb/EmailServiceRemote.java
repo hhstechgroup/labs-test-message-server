f2ab752b5414049903bcf0a45a7d18b6cde2ec93
artur.kandur <artur.kandur@enagagepoint.com>
2014-01-30 15:06:20 +0200

    NEW: NEW:LTMS-18 Back-end development - ejb
    Signed-off-by: artur.kandur <artur.kandur@enagagepoint.com>

---
+++ Messaging/messaging-ejb/src/main/java/com/engagepoint/university/messaging/ejb/EmailServiceRemote.java
@@ -0,0 +1,11 @@
+package com.engagepoint.university.messaging.ejb;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: KArt
+ * Date: 30.01.14
+ * Time: 14:42
+ * To change this template use File | Settings | File Templates.
+ */
+public interface EmailServiceRemote {
+}
