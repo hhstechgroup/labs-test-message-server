71516d2af7aadfca06426d5c322e4c83ef32efb5
Xilibit <alexey.danchuk@outlook.com>
2014-02-04 16:39:14 +0200

    CHANGE: Refactored EmailDAOImpl.java annotations according to persistence.xml and changed annotation to ViewScoped in EmailBean.java

--- src/main/java/com/engagepoint/university/messaging/beans/EmailBean.java
+++ src/main/java/com/engagepoint/university/messaging/beans/EmailBean.java
@@ -4,8 +4,7 @@ import com.engagepoint.university.messaging.dao.GenericDAO;
 import com.engagepoint.university.messaging.dto.EmailMessageDTO;
 
 import javax.annotation.PostConstruct;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
@@ -13,7 +12,7 @@ import java.util.Date;
 import java.util.List;
 
 @Named
-@RequestScoped
+@ViewScoped
 public class EmailBean implements Serializable {
 
     @Inject
--- src/main/java/com/engagepoint/university/messaging/dao/impl/EmailDAOImpl.java
+++ src/main/java/com/engagepoint/university/messaging/dao/impl/EmailDAOImpl.java
@@ -4,82 +4,91 @@ import com.engagepoint.university.messaging.dao.GenericDAO;
 import com.engagepoint.university.messaging.dto.EmailMessageDTO;
 
 
+import javax.annotation.Resource;
 import javax.faces.bean.RequestScoped;
-import javax.inject.Inject;
 import javax.persistence.*;
-import javax.transaction.Transactional;
-import javax.transaction.UserTransaction;
+import javax.transaction.*;
 import java.util.List;
 
 @RequestScoped
 public class EmailDAOImpl implements GenericDAO {
 
-//    @PersistenceContext(unitName = "messaging", type = PersistenceContextType.EXTENDED)
-    private EntityManager em;
+    // For server-side JPA realization.
+    // @Resource(name="java:comp/env/UserTransaction")
+    // UserTransaction userTransaction;
 
-//    @Inject
-//    private UserTransaction utx;
+    // @PersistenceContext(unitName = "messaging", type = PersistenceContextType.EXTENDED)
+    @PersistenceUnit(unitName = "messaging")
+    private EntityManagerFactory emf;
+    private EntityManager entityManager;
+
     public EmailDAOImpl() {
-        em = Persistence.createEntityManagerFactory("messaging").createEntityManager();
+        emf = Persistence.createEntityManagerFactory("messaging");
+        entityManager = emf.createEntityManager();
     }
 
     @Override
-//    @Transactional
+    @Transactional
     public EmailMessageDTO getEmail(int id) {
-        em.getTransaction().begin();
+        //EntityManager entityManager = emf.createEntityManager();
         EmailMessageDTO email = null;
+        entityManager.getTransaction().begin();
         try {
-            email = em.createNamedQuery(EmailMessageDTO.GET_EMAIL_BY_ID,EmailMessageDTO.class).setParameter("id", id).getSingleResult();
+            email = entityManager.createNamedQuery(EmailMessageDTO.GET_EMAIL_BY_ID,EmailMessageDTO.class).setParameter("id", id).getSingleResult();
         } catch (NoResultException e) {
             email = null;
         }
-        em.getTransaction().commit();
+        entityManager.getTransaction().commit();
         return email;
     }
 
     @Override
-//    @Transactional
+    @Transactional
     public List<EmailMessageDTO> getEmailBySender(String sender) {
-        em.getTransaction().begin();
-        List<EmailMessageDTO> emails = em.createNamedQuery(EmailMessageDTO.GET_EMAIL_BY_SENDER, EmailMessageDTO.class).setParameter("sender", sender).getResultList();
-        em.getTransaction().commit();
+        //EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        List<EmailMessageDTO> emails = entityManager.createNamedQuery(EmailMessageDTO.GET_EMAIL_BY_SENDER, EmailMessageDTO.class).setParameter("sender", sender).getResultList();
+        entityManager.getTransaction().commit();
         return emails;
-
     }
 
     @Override
-//    @Transactional
+    @Transactional
     public List<EmailMessageDTO> getEmailByReceiver(String receiver) {
-        em.getTransaction().begin();
-        List<EmailMessageDTO> emails = em.createNamedQuery(EmailMessageDTO.GET_EMAIL_BY_RECEIVER, EmailMessageDTO.class).setParameter("receiver", receiver).getResultList();
-        em.getTransaction().commit();
+        //EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        List<EmailMessageDTO> emails = entityManager.createNamedQuery(EmailMessageDTO.GET_EMAIL_BY_RECEIVER, EmailMessageDTO.class).setParameter("receiver", receiver).getResultList();
+        entityManager.getTransaction().commit();
         return emails;
 
     }
 
     @Override
-//    @Transactional
+    @Transactional
     public List<EmailMessageDTO> getAllEmails() {
-        em.getTransaction().begin();
-        List<EmailMessageDTO> emails = em.createNamedQuery(EmailMessageDTO.GET_ALL, EmailMessageDTO.class).getResultList();
-        em.getTransaction().commit();
+        //EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        List<EmailMessageDTO> emails = entityManager.createNamedQuery(EmailMessageDTO.GET_ALL, EmailMessageDTO.class).getResultList();
+        entityManager.getTransaction().commit();
         System.out.println(emails);
         return emails;
     }
 
     @Override
-//    @Transactional
+    @Transactional
     public void saveEmail(EmailMessageDTO email) {
-        em.getTransaction().begin();
-        em.persist(email);
-        em.getTransaction().commit();
+        //EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(email);
+        entityManager.getTransaction().commit();
     }
 
     @Override
-//    @Transactional
+    @Transactional
     public void deleteEmail(int id) {
-        em.getTransaction().begin();
-        em.remove(em.find(EmailMessageDTO.class, id));
-        em.getTransaction().commit();
+        //EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.remove(entityManager.find(EmailMessageDTO.class, id));
+        entityManager.getTransaction().commit();
     }
 }
