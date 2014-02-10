f21c3bf1244628b1d4a1a0b9c1974aba68693916
arturkandur <arturkandur@gmail.com>
2014-02-10 18:08:57 +0200

    FIX: Add header
    Signed-off-by: arturkandur <arturkandur@gmail.com>

--- src/main/java/com/engagepoint/university/messaging/beans/EmailBean.java
+++ src/main/java/com/engagepoint/university/messaging/beans/EmailBean.java
@@ -1,7 +1,6 @@
 package com.engagepoint.university.messaging.beans;
 
 import com.engagepoint.university.messaging.dao.condao.EmailDAO;
-import com.engagepoint.university.messaging.dto.EmailDTO;
 import com.engagepoint.university.messaging.entities.Email;
 
 import javax.annotation.PostConstruct;
@@ -9,7 +8,6 @@ import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -23,35 +21,20 @@ public class EmailBean implements Serializable {
     @Inject
     private EmailDAO emailDAO;
 
-    @PostConstruct
-    public void init() {
-        EmailDTO newEmailDTO = new EmailDTO();
-        newEmailDTO.setSender("author 1");
-        newEmailDTO.setSubject("Subject: Hello from author 1.");
-        newEmailDTO.setBody("Email body from author 1.");
-        newEmailDTO.setSendDate(new Date(12315612L));
-        newEmailDTO.setDeliveryDate(new Date(123425671L));
+    private List<Email> myList;
 
-        EmailDTO newEmailDTO1 = new EmailDTO();
-        newEmailDTO1.setSender("author 2");
-        newEmailDTO1.setSubject("Subject: Hello from author 2.");
-        newEmailDTO1.setBody("Email body from author 2.");
-        newEmailDTO1.setSendDate(new Date(12344678L));
-        newEmailDTO1.setDeliveryDate(new Date(123456789L));
 
-        EmailDTO newEmailDTO2 = new EmailDTO();
-        newEmailDTO2.setSender("author 3");
-        newEmailDTO2.setSubject("Subject: Hello from author 3.");
-        newEmailDTO2.setBody("Email body from author 3.");
-        newEmailDTO2.setSendDate(new Date(12343678L));
-        newEmailDTO2.setDeliveryDate(new Date(123446789L));
+    public void setMyList(List<Email> emails) {
 
-        emailDAO.save(new Email(newEmailDTO));
-        emailDAO.save(new Email(newEmailDTO1));
-        emailDAO.save(new Email(newEmailDTO2));
+        this.myList = emails;
     }
 
-    public List<Email> getAllEmails(){
-        return emailDAO.getAll();
+    public List<Email> getMyList(){
+        if (myList==null)
+                return emailDAO.getAll();
+
+        return myList;
     }
+
+
 }
