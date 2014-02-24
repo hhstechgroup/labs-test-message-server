4969ec1356a94b1fee2d9666e4556347001a65f9
Miklosh <nkozhumaka@gmail.com>
2014-02-25 00:27:38 +0200

    NEW: added server information server tab

--- src/main/java/com/engagepoint/university/messaging/services/ServerInfoService.java
+++ src/main/java/com/engagepoint/university/messaging/services/ServerInfoService.java
@@ -1,19 +1,30 @@
-//package com.engagepoint.university.messaging.services;
-//
-//import com.engagepoint.university.messaging.smpp.SMPPServerMain;
-//
-//import javax.faces.view.ViewScoped;
-//import javax.inject.Inject;
-//import javax.inject.Named;
-//
-//@Named
-//@ViewScoped
-//public class ServerInfoService {
-//
-//    @Inject
-//    SMPPServerMain serverSMPP;
-//    @Inject
-//    InitService initService;
-//
-//
-//}
+package com.engagepoint.university.messaging.services;
+
+import com.engagepoint.university.messaging.smpp.SMPPServerMain;
+import com.engagepoint.university.messaging.smtp.SMTPServerMain;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class ServerInfoService {
+
+    @Inject
+    SMPPServerMain serverSMPP;
+    @Inject
+    SMTPServerMain serverSMTP;
+
+    public String getSMTPhostName () {
+        return serverSMTP.getHostname();
+    }
+
+    public int getSMTPport () {
+        return serverSMTP.getPort();
+    }
+
+    public int getSMPPport () {
+        return serverSMPP.getPort();
+    }
+}
