67223242a5ad4318df38d3c903c32817b5fdf805
arturkandurengagepoint <artur.kandur@engagepoint.com>
2014-02-26 16:31:45 +0200

    CHANGE: Clean branch
    Signed-off-by: arturkandurengagepoint <artur.kandur@engagepoint.com>

--- src/main/java/com/engagepoint/university/messaging/util/ToDtoConverter.java
+++
@@ -1,8 +0,0 @@
-package com.engagepoint.university.messaging.util;
-
-import com.engagepoint.university.messaging.dto.base.BaseDTO;
-import com.engagepoint.university.messaging.entities.base.BaseEntity;
-
-public interface ToDtoConverter<FROM_ENTITY extends BaseEntity, TO_DTO extends BaseDTO> {
-    public TO_DTO convert(FROM_ENTITY from);
-}
--- src/main/java/com/engagepoint/university/messaging/util/ToEntityConverter.java
+++
@@ -1,8 +0,0 @@
-package com.engagepoint.university.messaging.util;
-
-import com.engagepoint.university.messaging.dto.base.BaseDTO;
-import com.engagepoint.university.messaging.entities.base.BaseEntity;
-
-public interface ToEntityConverter<FROM_DTO extends BaseDTO, TO_ENTITY extends BaseEntity> {
-    public TO_ENTITY convert(FROM_DTO from);
-}
